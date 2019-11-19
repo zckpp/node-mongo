3955217739a8a95842ee0b993da86299069de0ad
Quinn <zckpp@qq.com>
2019-11-19 13:37:55 -0500

    fix .ignore

--- config/database.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    url: 'mongodb://admin:Sci3nc3ls@127.0.0.1:27017/cylinder_orders?authSource=admin'
-    // url: 'mongodb://127.0.0.1:27017/cylinder_orders'
-}
--- config/ssl.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    key: '/etc/pki/tls/digicert/star_carnegiescience_edu.key',
-    cert: '/etc/pki/tls/digicert/star_carnegiescience_edu.pem'
-}
